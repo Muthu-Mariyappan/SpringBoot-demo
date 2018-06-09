20eac468e9909f623d08a35e63143544d6682e8d
Muthu Mariyappan <ggmm.muthu@gmail.com>
2018-06-09 22:46:28 +0530

    Update BaseController.java

--- spring-base-rest-master/src/main/java/org/tact/base/rest/BaseController.java
+++ spring-base-rest-master/src/main/java/org/tact/base/rest/BaseController.java
@@ -1,5 +1,9 @@
 package org.tact.base.rest;
-
+/**
+*Author: Muthu Mariyappan
+*Date : 09/06/2018
+*Program Name: SpringBoot-demo-Reversing-String
+*/
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -10,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
-	
-	@RequestMapping(value = "/base")
+	/***Given method which prints default json object on visiting /base**/
+	@RequestMapping(value = "/base") //Maps /base call to listUsers method
     public Map<String,Object> listUsers() {
-        Map<String, Object> map = new LinkedHashMap<String, Object>();
+        Map<String, Object> map = new LinkedHashMap<String, Object>(); //Using linkedHashmap to preserve insertion order
         map.put("one", "two");
         map.put("three", "four");
         map.put("five", "six");
@@ -21,11 +25,11 @@ public class BaseController {
         return  map;
     }
 	
-	@RequestMapping(value = "/base/{name}")
-    public Map<String,Object> reverseListUsers(@PathVariable String name) {
+	@RequestMapping(value = "/base/{name}") // Maps /base with passed value as /{value}
+    public Map<String,Object> reverseListUsers(@PathVariable String name) { //@PathVariable to access the passed value
         Map<String, Object> map = new LinkedHashMap<String, Object>();
         map.put("name", name);
-        map.put("reversed_name", new StringBuilder(name).reverse().toString());
-        return  map;
+        map.put("reversed_name", new StringBuilder(name).reverse().toString()); // java StringBuilder method to reverse a String
+        return  map; // Returns the original and reversed string as a result
     }
 }
